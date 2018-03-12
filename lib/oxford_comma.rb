e47fc3c4035d96fb627ea04ce9678d3867ef7f0a
Patrick J Louis <lumenfuzz@gmail.com>
2018-03-12 12:16:12 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,12 @@
 def oxford_comma(array)
+  case array.length
+  when 0
+    nil
+  when 1
+    array.join
+  when 2
+    "#{array[0]} and #{array[1]}"
+  else
   last_e = array.pop
   main_e = array.join(", ")
   "#{main_e} and #{last_e}"
