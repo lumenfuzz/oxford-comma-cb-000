ee5b602ef835fd78cd693d038997958dddde93bd
Patrick J Louis <lumenfuzz@gmail.com>
2018-03-12 12:17:36 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  case array.length
+  when 0
+    nil
+  when 1
+    array.join
+  when 2
+    "#{array[0]} and #{array[1]}"
+  else
+    last_e = array.pop
+    main_e = array.join(", ")
+    "#{main_e}, and #{last_e}"
+  end
 end
